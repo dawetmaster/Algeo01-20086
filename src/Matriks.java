ba2addcce2fbbe2bed11e27b0efff9b7e1e20b69
alifiarahmah <alifiarahmah03@gmail.com>
2021-09-18 17:09:40 +0700

    added plusMinusMatriks

--- src/Matriks.java
+++ src/Matriks.java
@@ -7,31 +7,30 @@ public class Matriks {
     /* METHOD */
 
     // konstruktor
-    public Matriks(int nbaris,int nkolom){
-        /* Kontruktor matriks*/
+    public Matriks(int nbaris, int nkolom) {
+        /* Kontruktor matriks */
         this.Nbaris = nbaris;
         this.Nkolom = nkolom;
         this.matriks = new double[Nbaris][Nkolom];
-        //inisialisasi matriks
+        // inisialisasi matriks
         for (int i = 0; i < this.Nbaris; i++)
             for (int j = 0; j < this.Nkolom; j++)
                 matriks[i][j] = 0;
     }
     // Input/Output
-    /*NOTE: Belum bikin input dari GUI nya
-    public void readMatriks() {*/
-        /* I.S. semua elemen matriks bernilai 0*/
-        /* F.S. Matriks terisi elemen */
-     /*   for (int i = 0; i < this.Nbaris; i++)
-            for (int j = 0; j < this.Nkolom; j++)
-                //NOTE:isi dengan input
-                matriks[i][j] = 0;
-    }
-    */
+    /*
+     * NOTE: Belum bikin input dari GUI nya public void readMatriks() {
+     */
+    /* I.S. semua elemen matriks bernilai 0 */
+    /* F.S. Matriks terisi elemen */
+    /*
+     * for (int i = 0; i < this.Nbaris; i++) for (int j = 0; j < this.Nkolom; j++)
+     * //NOTE:isi dengan input matriks[i][j] = 0; }
+     */
 
-    public void writeMatriks() {//Note: Belum dikonekin sama gui
-        /* I.S. matriks terdefinisi*/
-        /* F.S. mencetak matriks ke layar*/
+    public void writeMatriks() {// Note: Belum dikonekin sama gui
+        /* I.S. matriks terdefinisi */
+        /* F.S. mencetak matriks ke layar */
         for (int i = 0; i < this.Nbaris; i++) {
             for (int j = 0; j < this.Nkolom; j++)
                 System.out.print(matriks[i][j] + " ");
@@ -40,7 +39,7 @@ public class Matriks {
     }
 
     /* Copy/Kloning matriks */
-    public Matriks cloneMatriks(){
+    public Matriks cloneMatriks() {
         Matriks m = new Matriks(this.Nbaris, this.Nkolom);
         for (int i = 0; i < m.Nbaris; i++) {
             m.matriks[i] = this.matriks[i].clone();
@@ -49,23 +48,25 @@ public class Matriks {
     }
 
     /* Predikat */
-    public boolean isIdentity(){
-        //menghasilkan true jika Matriks ini adalah matriks identitas,yakni elemen di diagonal utama semuanya bernilai true
-        //kamus lokal
-        boolean isIdentity;//menghasilkan true jika Matriks ini matriks identitas
-        int i,j;//variabel looping
-        //Algoritma
+    public boolean isIdentity() {
+        // menghasilkan true jika Matriks ini adalah matriks identitas,yakni elemen di
+        // diagonal utama semuanya bernilai true
+        // kamus lokal
+        boolean isIdentity;// menghasilkan true jika Matriks ini matriks identitas
+        int i, j;// variabel looping
+        // Algoritma
         isIdentity = true;
-        i=0;
-        j=0;
-        while(i<this.Nbaris && isIdentity){
-            while(j<this.Nkolom && isIdentity){
-                if(i==j) {//untuk elemen diagonal utama
-                    if(Double.compare(this.matriks[i][j],1.0)!=0)//kalau nilai nya sama dengan 0 berarti nilai si elemen matriksnya 1. Ada di dokumentasi fungsinya
+        i = 0;
+        j = 0;
+        while (i < this.Nbaris && isIdentity) {
+            while (j < this.Nkolom && isIdentity) {
+                if (i == j) {// untuk elemen diagonal utama
+                    if (Double.compare(this.matriks[i][j], 1.0) != 0)// kalau nilai nya sama dengan 0 berarti nilai si
+                                                                     // elemen matriksnya 1. Ada di dokumentasi
+                                                                     // fungsinya
                         isIdentity = false;
-                }
-                else{
-                    if(this.matriks[i][j]!=0){
+                } else {
+                    if (this.matriks[i][j] != 0) {
                         isIdentity = false;
                     }
                 }
@@ -75,24 +76,44 @@ public class Matriks {
         }
         return isIdentity;
     }
-    /* Fungsi dan Prosedur*/
-    //* TODO: cek fungsi ini bila sudah ada fungsi untuk input
-    public static Matriks kali(Matriks m1,Matriks m2){
-        //fungsi yang melakukan perkalian matriks antara m1 dan m2
-        //prekondisi: m1.NBaris = m2.NKolom
-        Matriks mat = new Matriks(m1.Nbaris,m2.Nkolom);
-        for(int i=0;i<mat.Nbaris;i++){
-            for(int j=0;j<mat.Nkolom;j++){
+
+    public Matriks plusMinusMatriks(Matriks m1, Matriks m2, boolean isPlus) {
+        // m1.Nbaris == m2.Nbaris dan m1.Nkolom = m2.Nkolom
+        // return m1 + m2 jika isPlus = true, dan m1 - m2 jika isPlus = false.
+        Matriks m = new Matriks(m1.Nbaris, m1.Nkolom);
+        if (isPlus) {
+            for (int i = 0; i < m1.Nbaris; i++) {
+                for (int j = 0; j < m1.Nkolom; j++) {
+                    m.matriks[i][j] = m1.matriks[i][j] + m2.matriks[i][j];
+                }
+            }
+        }
+        for (int i = 0; i < m1.Nbaris; i++) {
+            for (int j = 0; j < m1.Nkolom; j++) {
+                m.matriks[i][j] = m1.matriks[i][j] + m2.matriks[i][j];
+            }
+        }
+        return m;
+    }
+
+    /* Fungsi dan Prosedur */
+    // * TODO: cek fungsi ini bila sudah ada fungsi untuk input
+    public static Matriks kali(Matriks m1, Matriks m2) {
+        // fungsi yang melakukan perkalian matriks antara m1 dan m2
+        // prekondisi: m1.NBaris = m2.NKolom
+        Matriks mat = new Matriks(m1.Nbaris, m2.Nkolom);
+        for (int i = 0; i < mat.Nbaris; i++) {
+            for (int j = 0; j < mat.Nkolom; j++) {
                 double sum_ = 0;
-                for(int k=0;k<m1.Nkolom;k++)
-                    sum_+= (m1.matriks[i][k] * m2.matriks[k][j]);
+                for (int k = 0; k < m1.Nkolom; k++)
+                    sum_ += (m1.matriks[i][k] * m2.matriks[k][j]);
                 mat.matriks[i][j] = sum_;
             }
         }
         return mat;
     }
 
-    public double determinantReduction(){
+    public double determinantReduction() {
         /* I.S. jumlah baris dan jumlah kolom harus sama */
         /* F.S. diperoleh determinan */
         /* kloning matriks dulu */
@@ -122,13 +143,13 @@ public class Matriks {
         }
     }
 
-    public Matriks minor(int row, int col){
+    public Matriks minor(int row, int col) {
         if (this.Nbaris == this.Nkolom) {
             int dimension = this.Nbaris - 1;
             Matriks minor = new Matriks(dimension, dimension);
             for (int i = 0; i < dimension; i++) {
                 for (int j = 0; j < dimension; j++) {
-                    minor.matriks[i][j] = this.matriks[i >= row ? i+1 : i][j >= col ? j+1 : j];
+                    minor.matriks[i][j] = this.matriks[i >= row ? i + 1 : i][j >= col ? j + 1 : j];
                 }
             }
             return minor;
@@ -137,7 +158,7 @@ public class Matriks {
         }
     }
 
-    public double determinantCofactor(){
+    public double determinantCofactor() {
         /* I.S. jumlah baris dan jumlah kolom harus sama */
         /* F.S. diperoleh determinan */
         /* Pakai default value, kolom 1 */
@@ -150,7 +171,7 @@ public class Matriks {
                 // rekurens
                 double determinant = 0;
                 for (int i = 0; i < this.Nbaris; i++) {
-                    determinant += Math.pow(-1, i+1) * this.minor(i, 1).determinantCofactor();
+                    determinant += Math.pow(-1, i + 1) * this.minor(i, 1).determinantCofactor();
                 }
                 return determinant;
             }
@@ -159,7 +180,7 @@ public class Matriks {
         }
     }
 
-    public double determinantCofactor(int mat_index, boolean row_mode){
+    public double determinantCofactor(int mat_index, boolean row_mode) {
         /* I.S. jumlah baris dan jumlah kolom harus sama */
         /* F.S. diperoleh determinan */
         /* Prekondisi: ada input mat_index dan row_mode */
@@ -173,11 +194,11 @@ public class Matriks {
                 double determinant = 0;
                 if (row_mode) {
                     for (int i = 0; i < this.Nkolom; i++) {
-                        determinant += Math.pow(-1, i+1) * this.minor(mat_index, i).determinantCofactor();
+                        determinant += Math.pow(-1, i + 1) * this.minor(mat_index, i).determinantCofactor();
                     }
                 } else {
                     for (int i = 0; i < this.Nbaris; i++) {
-                        determinant += Math.pow(-1, i+1) * this.minor(i, mat_index).determinantCofactor();
+                        determinant += Math.pow(-1, i + 1) * this.minor(i, mat_index).determinantCofactor();
                     }
                 }
                 return determinant;
