2c1b9e238bf74faf7cfc9676fad5a8a76afeff91
Wiradhika6051 <16520059@std.stei.itb.ac.id>
2021-09-27 22:59:49 +0700

    Menambahkan fitur baca dari file untuk mencari determinan

--- src/DeterminanERO.java
+++ src/DeterminanERO.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 
 public class DeterminanERO {
     private JLabel eroDetLabel;
@@ -10,9 +13,54 @@ public class DeterminanERO {
     private JButton hitungButton;
     private JButton bukaFileButton;
     private JLabel resultLabel;
-    private JLabel result;
+    private JLabel resultField;
     private JPanel eroDeterminan;
     private JFrame eroDeterminanFrame = new JFrame("Kalkulator Matriks");
+
+    public DeterminanERO() {
+        bukaFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser("./test");
+                int result = fileChooser.showOpenDialog(eroDeterminanFrame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    System.out.println(selectedFile.getName());
+                    //parse input
+                    String isi_file = "";
+                    try {
+                        String line = null;
+                        BufferedReader reader;
+                        reader = new BufferedReader(new FileReader(selectedFile));
+                        while((line=reader.readLine())!=null){
+                            isi_file+=line;
+                            isi_file+="\n";
+                        }
+                        String[] augmented = isi_file.split("\n");
+                        if(augmented.length>0){
+                            String text_mat = "";
+                            inputN.setText(Integer.toString(augmented.length));
+                            for(int i=0;i<augmented.length;i++) {
+                                String[] augmented_baris = augmented[i].split(" ");
+                                for(int j=0;j<augmented_baris.length;j++){
+                                    inputMatrix.append(augmented_baris[j]+" ");
+                                }
+                                inputMatrix.append("\n");
+                            }
+                        }
+                    }
+                    catch (FileNotFoundException fnf){
+                        resultField.setText("File tidak ditemukan");
+                    }
+                    catch (IOException io){
+                        resultField.setText("File kosong!");
+                    }
+
+                }
+            }
+        });
+    }
+
     public void run(){
         eroDeterminanFrame.setContentPane(new DeterminanERO().eroDeterminan);
         eroDeterminanFrame.setMinimumSize(new Dimension(800,400));
