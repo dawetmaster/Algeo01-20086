5103cbf59c5a49fce68efd64ca51c7d5cbca4177
dawetmaster <andika.naufal48@gmail.com>
2021-09-29 20:35:21 +0700

    gui regresi komplit

--- src/RegresiForm.java
+++ src/RegresiForm.java
@@ -1,5 +1,28 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class RegresiForm {
+    private JFrame regresiFrame = new JFrame("Regresi Linear Berganda");
+    private JPanel regresiPanel;
+    private JTextField varField;
+    private JTextField countDataField;
+    private JButton readfileButton;
+    private JTextArea inputArea;
+    private JTextArea splTextArea;
+    private JTextArea resultTextArea;
+    private JButton saveButton;
+    private JLabel titleLabel;
+    private JLabel guideLabel;
+    private JLabel varLabel;
+    private JLabel countDataLabel;
+    private JLabel inputDataLabel;
+    private JLabel resultsplLabel;
+    private JLabel resultLabel;
 
+    public void run() {
+        regresiFrame.setContentPane(new RegresiForm().regresiPanel);
+        regresiFrame.setMinimumSize(new Dimension(800, 400));
+        regresiFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        regresiFrame.setVisible(true);
+    }
 }
