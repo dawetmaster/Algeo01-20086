6ecf62b7ee1e2598e54342976404d80fb4a3cde4
Wiradhika6051 <16520059@std.stei.itb.ac.id>
2021-09-30 15:34:45 +0700

    pindahin sementara

--- src/CramerMethod.java
+++ src/CramerMethod.java
@@ -1,10 +1,10 @@
 public class CramerMethod {
 
     public static Matriks solve(Matriks matA, Matriks matB) {
-        //menyelesaikan SPL menggunakan metode cramer dan mencari determinan menggunakan reduksi baris
+        //menyelesaikan SPL menggunakan metode cramer dan mencari determinan menggunakan ekspansi kofaktor
         Matriks hasil = new Matriks(matA.Nbaris,1);
         double detMat;
-        detMat = matA.determinantReduction();
+        detMat = matA.determinantCofactor();
         if(detMat==0){
             return null;
         }
@@ -16,9 +16,9 @@ public class CramerMethod {
                 System.out.println(temp.matriks[i][j]);
             }
             temp.writeMatriks();
-            System.out.println("detA:"+temp.determinantReduction());
+            System.out.println("detA:"+temp.determinantCofactor());
             System.out.println("detB:"+detMat);
-            hasil.matriks[j][0] = temp.determinantReduction()/detMat;
+            hasil.matriks[j][0] = temp.determinantCofactor()/detMat;
            // System.out.println("Hasil:"+hasil.matriks[j][0]);
         }
         return hasil;
