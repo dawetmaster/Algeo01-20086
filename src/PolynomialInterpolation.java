e7986453a21ce6de1c7b7b847b64dc3289213a60
dawetmaster <andika.naufal48@gmail.com>
2021-10-01 16:03:11 +0700

    minor add lib java.lang.*

--- src/PolynomialInterpolation.java
+++ src/PolynomialInterpolation.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.lang.*;
 
 public class PolynomialInterpolation {
     Matriks m;
